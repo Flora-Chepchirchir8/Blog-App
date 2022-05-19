05637fde0188a629d082a42e6b20f852669762cc
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-05-19 09:42:43 +0300

    remove start.sh from version control

--- start.sh
+++
@@ -1,8 +0,0 @@
-export SECRET_KEY='Hyu782DBACdGn5ZGdfyjkYlhSd@JS70'
-export MAIL_USERNAME='florachirry80@gmail.com'
-export MAIL_PASSWORD='@Access80'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:felix@localhost/flora'
-
-
-
-python3 manage.py server 
